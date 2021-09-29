8b76aee58f80b3ea14c491b71cac8d71eaec1d66
grantgolden13 <grantgolden13@gmail.com>
2021-09-28 17:47:44 -0700

    finish working console game

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 // Need a function called computerPlay that randomly returns either 'Rock', 'Paper', or 'Scissors'. we'll use this function to make the computer's play
 function computerPlay() {
-    let computerSelection = Math.random();
-    if (computerSelection > 0.666666667) {
-        return "Rock";
-    } else if (computerSelection >= 0.333333333 && computerSelection <= 0.666666667) {
-        return "Paper";
+    const x = Math.random();
+    if (x > 0.666666667) {
+        return "rock";
+    } else if (x >= 0.333333333 && x <= 0.666666667) {
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
@@ -15,34 +15,49 @@ function computerPlay() {
 // we want to return the results of this function call, not console.log it.
 
 function rockPaperScissors(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
+    if (playerSelection === computerSelection) {
         return "It's a tie!";
-    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
-        return "You Lose! Paper beats Rock";
-    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        return "You Win! Rock beats Scissors";
-    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-        return "You Win! Paper beats Rock";
-    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-        return "You Lose! Scissors beats Paper";
-    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-        return "You Lose! Rock beats Scissors";
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        return "You Win! Scissors beats Paper";
+    } else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
+        return "You Lose!";
+    } else {
+        return "You Win!";
     }
 }
 
-// then write a new function called game(). use the previous function inside of this one and play to a 5 round game that keeps score and reports a winner/loser at the end
-// don't make this a loop since I haven't learned loops yet
-// for this we'll use console.log to display the results of each round and the winner at the end
-// use prompt() to get input from the user
-// feel free to create more functions if necessary
+// function to get a value from user and validate the input on the playerSelection 
 
+function playerSubmission() {
+    let playerSelection = prompt("Rock, Paper, or Scissors?").toString().toLowerCase();
+    if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
+        alert("Only valid options are rock, paper, or scissors.");
+        playerSubmission();                             //  necessary to run prompt again after user makes error
+        }
+          else {
+            return playerSelection;
+          }
+}
 
-
-// declare function game and make it: receive user input via prompt. take said input and pass it as a value to rockPaperScissors.
+// then write a new function called game(). use rockPaperScissors function inside of this one and play to a 5 round game that keeps score and reports a winner/loser at the end
 
 function game() {
-    let playerSelection = prompt("Choose your fighter: Type 'rock', 'paper', or 'scissors' into the field below.");
-    rockPaperScissors(playerSelection, computerSelection);
+    let playerWins = 0;
+    let computerWins = 0;
+    while ((playerWins < 5) && (computerWins < 5)) {
+        console.log("You have won: " + playerWins + ". Computer has won: " + computerWins);
+        playerSelection = playerSubmission();
+        computerSelection = computerPlay();
+        let result = rockPaperScissors(playerSelection, computerSelection);
+        if (result === "You Win!") {
+            playerWins++;
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        } else if (result === "You Lose") {
+            computerWins++
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        } else {
+            console.log(`Tied! You chose ${playerSelection}, computer chose ${computerSelection}`)
+        }
+    }
 }
+
+game();                             //  must be included to run on start
+
